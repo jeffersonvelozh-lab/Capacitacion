77679858a694546efb752164503ec0fcc91f6803
jeffersonveloz-lab <jeffersonvelozh@gmail.com>
2026-07-19 11:50:22 -0500

    Borrando

--- Cajero-practica/Types.ts
+++
@@ -1,8 +0,0 @@
-import type Dinero = require("./Interfaces");
-
-
-export type TipoTransaccion = 'deposito' | 'retiro';
-
-export type ResultadoOperacion =
-    | { tipo: "exito"; mensaje: string; saldoActual: Dinero.Dinero }
-    | { tipo: "error"; mensaje: string };
